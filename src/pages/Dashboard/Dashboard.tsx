8c9d6af42ad7de462aabdd393c4e655301f37370
Danek <danek.fill@outlook.com>
2025-02-03 13:52:34 -0500

    add tailwindcss, add basic dash skeleton

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,26 @@
 
+type moduleComponent = {
+  name: string,
+}
+
 const Dashboard = () => {
+
+  const dashboardModules: moduleComponent[] = [{name: '1'},{name: '2'},{name: '3'},{name: '4'},];
+
+  const moduleComponents = dashboardModules.map( (module) => {
+    return(
+      <li className="module">{module.name}</li>
+    )
+  })
   
   return (
     <div className="dashboard">
-      Dashboard
+      <h1>Dashboard</h1>
+      <div className="modules flex items-center">
+        <ul className="flex">
+          {moduleComponents}
+        </ul>
+      </div>
     </div>
   )
 };
